3784766bd485016fce69b98c5f69337bb640dc85
John Solly <9572232+jsolly@users.noreply.github.com>
2025-01-01 17:38:54 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,9 @@ import alpinejs from "@astrojs/alpinejs";
 
 import vue from "@astrojs/vue";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap(), alpinejs(), vue()],
+	integrations: [tailwind(), sitemap(), alpinejs(), vue(), react()],
 });
