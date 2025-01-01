7ff9fa66fc3917f3f552f172adb17bfff9f6ded9
John Solly <9572232+jsolly@users.noreply.github.com>
2025-01-01 18:32:39 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://checkboxes.xyz",
 	integrations: [tailwind(), sitemap(), alpinejs(), vue(), react(), svelte()],
 });
