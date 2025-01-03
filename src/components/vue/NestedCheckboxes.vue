c425e4332467f7990a6b9331fdddb626b226b302
John Solly <9572232+jsolly@users.noreply.github.com>
2025-01-02 19:02:11 -0500

    .

--- src/components/vue/NestedCheckboxes.vue
+++ src/components/vue/NestedCheckboxes.vue
@@ -44,7 +44,7 @@ const areSomeChildrenChecked = computed(() =>
 </script>
 
 <template>
-    <div>
+    <div class="px-3 py-2">
         <div>
             <!-- Parent Checkbox -->
             <div class="p-2 rounded-lg hover:bg-slate-100 transition-colors">
